bc8c580958a45c419c1d58ed400d19a558381f04
Ksenia Timokhina <kseenina@gmail.com>
2026-09-11 14:45:13 +0300

    Component ProjectList realized

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -1,3 +1,11 @@
-export default function ProjectList() {
-    
+export default function ProjectList({ projects }) {
+    return (
+        <div className="project-list">
+            {projects.map(project => (
+                <div className="project-card">
+                    <img src={project.img} alt={ project.category } />
+                </div>
+            ))}
+        </div>
+    );
 }
